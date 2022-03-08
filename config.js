67e5d918467d3932aaeadb22595e4b860eedfac7
nuzzzen <39726534+nuzzzen@users.noreply.github.com>
2022-03-08 12:05:15 +0100

    Customized config.js
    Added my preferences to config.js.

--- config.js
+++ config.js
@@ -11,29 +11,29 @@ const CONFIG = {
   // └─┘┴ ┴└─┘┴└─┘└─┘
 
   // General
-  name: 'John',
+  name: "Rick",
   imageBackground: false,
   openInNewTab: true,
   twelveHourFormat: false,
 
   // Greetings
-  greetingMorning: 'Good morning!',
-  greetingAfternoon: 'Good afternoon,',
-  greetingEvening: 'Good evening,',
-  greetingNight: 'Go to Sleep!',
+  greetingMorning: "Good morning!",
+  greetingAfternoon: "Good afternoon,",
+  greetingEvening: "Good evening,",
+  greetingNight: "Go to Sleep!",
 
   // Layout
-  bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
+  bentoLayout: "bento", // 'bento', 'lists', 'buttons'
 
   // Weather
-  weatherKey: 'InsertYourAPIKeyHere123456', // Write here your API Key
-  weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
-  weatherUnit: 'C', // 'F', 'C'
-  language: 'en', // More languages in https://openweathermap.org/current#multi
+  weatherKey: "InsertYourAPIKeyHere123456", // Write here your API Key
+  weatherIcons: "OneDark", // 'Onedark', 'Nord', 'Dark', 'White'
+  weatherUnit: "C", // 'F', 'C'
+  language: "en", // More languages in https://openweathermap.org/current#multi
 
   trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
-  defaultLatitude: '37.775',
-  defaultLongitude: '-122.419',
+  defaultLatitude: "37.775",
+  defaultLongitude: "-122.419",
 
   // Autochange
   autoChangeTheme: true,
@@ -43,8 +43,8 @@ const CONFIG = {
 
   // Autochange by hour options (24hrs format, string must be in: hh:mm)
   changeThemeByHour: false,
-  hourDarkThemeActive: '18:30',
-  hourDarkThemeInactive: '07:00',
+  hourDarkThemeActive: "18:30",
+  hourDarkThemeInactive: "07:00",
 
   // ┌┐ ┬ ┬┌┬┐┌┬┐┌─┐┌┐┌┌─┐
   // ├┴┐│ │ │  │ │ ││││└─┐
@@ -52,79 +52,79 @@ const CONFIG = {
 
   firstButtonsContainer: [
     {
-      id: '1',
-      name: 'Github',
-      icon: 'github',
-      link: 'https://github.com/',
+      id: "1",
+      name: "Reddit",
+      icon: "glasses",
+      link: "https://reddit.com/",
     },
     {
-      id: '2',
-      name: 'Mail',
-      icon: 'mail',
-      link: 'https://mail.protonmail.com/',
+      id: "2",
+      name: "ProtonMail",
+      icon: "mail",
+      link: "https://mail.protonmail.com/",
     },
     {
-      id: '3',
-      name: 'Todoist',
-      icon: 'trello',
-      link: 'https://todoist.com',
+      id: "3",
+      name: "YouTube",
+      icon: "youtube",
+      link: "https://youtube.com",
     },
     {
-      id: '4',
-      name: 'Calendar',
-      icon: 'calendar',
-      link: 'https://calendar.google.com/calendar/r',
+      id: "4",
+      name: "Calendar",
+      icon: "calendar",
+      link: "https://calendar.google.com/calendar/r",
     },
     {
-      id: '5',
-      name: 'Reddit',
-      icon: 'glasses',
-      link: 'https://reddit.com',
+      id: "5",
+      name: "Twitch",
+      icon: "twitch",
+      link: "https://twitch.com",
     },
     {
-      id: '6',
-      name: 'Odysee',
-      icon: 'youtube',
-      link: 'https://odysee.com/',
+      id: "6",
+      name: "Ingegneria Informatica - Roma Tre (Moodle)",
+      icon: "graduation-cap",
+      link: "https://ingegneria.el.uniroma3.it/",
     },
   ],
 
   secondButtonsContainer: [
     {
-      id: '1',
-      name: 'Music',
-      icon: 'headphones',
-      link: 'https://open.spotify.com',
+      id: "1",
+      name: "Music",
+      icon: "headphones",
+      link: "https://open.spotify.com",
     },
     {
-      id: '2',
-      name: 'twitter',
-      icon: 'twitter',
-      link: 'https://twitter.com/',
+      id: "2",
+      name: "twitter",
+      icon: "twitter",
+      link: "https://twitter.com/",
     },
     {
-      id: '3',
-      name: 'bot',
-      icon: 'bot',
-      link: 'https://discord.com/app',
+      id: "3",
+      name: "bot",
+      icon: "bot",
+      link: "https://discord.com/app",
     },
     {
-      id: '4',
-      name: 'Amazon',
-      icon: 'shopping-bag',
-      link: 'https://amazon.com/',
+      id: "4",
+      name: "Amazon",
+      icon: "shopping-bag",
+      link: "https://amazon.com/",
     },
     {
-      id: '5',
-      name: 'Hashnode',
-      icon: 'pen-tool',
-      link: 'https://hashnode.com/',
+      id: "5",
+      name: "Hashnode",
+      icon: "pen-tool",
+      link: "https://hashnode.com/",
     },
     {
-      id: '6',
-      name: 'Figma',
-      icon: 'figma',
-      link: 'https://figma.com/',
+      id: "6",
+      name: "Figma",
+      icon: "figma",
+      link: "https://figma.com/",
     },
   ],
 
@@ -135,46 +135,46 @@ const CONFIG = {
   // First Links Container
   firstlistsContainer: [
     {
-      icon: 'music',
-      id: '1',
+      icon: "music",
+      id: "1",
       links: [
         {
-          name: 'Inspirational',
-          link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+          name: "Inspirational",
+          link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
         },
         {
-          name: 'Classic',
-          link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+          name: "Classic",
+          link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
         },
         {
-          name: 'Oldies',
-          link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+          name: "Oldies",
+          link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
         },
         {
-          name: 'Rock',
-          link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+          name: "Rock",
+          link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
         },
       ],
     },
     {
-      icon: 'coffee',
-      id: '2',
+      icon: "coffee",
+      id: "2",
       links: [
         {
-          name: 'Linkedin',
-          link: 'https://www.linkedin.com',
+          name: "Linkedin",
+          link: "https://www.linkedin.com",
         },
         {
-          name: 'Dribbble',
-          link: 'https://www.dribbble.com',
+          name: "Gomp",
+          link: "https://gomp.uniroma3.it/",
         },
         {
-          name: 'Trello',
-          link: 'https://www.trello.com',
+          name: "GitLab",
+          link: "https://www.gitlab.com",
         },
         {
-          name: 'Slack',
-          link: 'https://www.slack.com',
+          name: "AUR",
+          link: "https://www.aur.archlinux.org.com",
         },
       ],
     },
@@ -183,46 +183,46 @@ const CONFIG = {
   // Second Links Container
   secondListsContainer: [
     {
-      icon: 'binary',
-      id: '1',
+      icon: "binary",
+      id: "1",
       links: [
         {
-          name: 'Spotify',
-          link: 'https://www.spotify.com',
+          name: "Spotify",
+          link: "https://www.spotify.com",
         },
         {
-          name: 'Reddit',
-          link: 'https://www.reddit.com',
+          name: "Reddit",
+          link: "https://www.reddit.com",
         },
         {
-          name: 'Hashnode',
-          link: 'https://www.hashnode.com',
+          name: "Hashnode",
+          link: "https://www.hashnode.com",
         },
         {
-          name: 'Pocket',
-          link: 'https://www.pocket.com',
+          name: "Pocket",
+          link: "https://www.pocket.com",
         },
       ],
     },
     {
-      icon: 'github',
-      id: '2',
+      icon: "github",
+      id: "2",
       links: [
         {
-          name: 'Front',
-          link: 'https://www.reddit.com/r/Frontend/',
+          name: "Front",
+          link: "https://www.reddit.com/r/Frontend/",
         },
         {
-          name: 'Rust',
-          link: 'https://www.reddit.com/r/rust/',
+          name: "Rust",
+          link: "https://www.reddit.com/r/rust/",
         },
         {
-          name: 'Go',
-          link: 'https://www.reddit.com/r/golang/',
+          name: "Go",
+          link: "https://www.reddit.com/r/golang/",
         },
         {
-          name: 'Repos',
-          link: 'https://github.com/migueravila',
+          name: "Repos",
+          link: "https://github.com/migueravila",
         },
       ],
     },
